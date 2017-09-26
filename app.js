57596b3fc4d3972980b94f487ed9be1b100f0813
petr-bazant <petr.bazant@outlook.com>
2017-09-26 14:02:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var http = require('http');
 var server = http.createServer(function(req, res){
-	res.writeHead(301, {'Location' : 'http://www.100pge.com'});
-	res.end();
+	if(req.url.includes('.cz')) {
+		res.writeHead(301, {'Location' : 'http://www.100pge.com/cz'});
+		res.end(); 
+	} else {
+		res.writeHead(301, {'Location' : 'http://www.100pge.com'});
+		res.end(); 
+	}
 });
 server.listen(process.env.PORT || 8085);
