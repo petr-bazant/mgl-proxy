8705df380b65c50729920dd1a6e3afd94ca02adc
petr-bazant <petr.bazant@outlook.com>
2017-09-26 13:48:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,6 @@
-//Lets require/import the HTTP module
 var http = require('http');
-var AWS = require('aws-sdk');
-
-AWS.config.update({
-    "secretAccessKey": "...",
-    "sessionToken": "...",
-    "accessKeyId": "...",
-    "region": "eu-central-1"
+var server = http.createServer(function(req, res){
+	res.writeHead(301, {'Location' : 'http://www.100pge.com'});
+	res.end();
 });
-
-var sns = new AWS.SNS();
-
-sns.subscribe({
-    Protocol: 'http',
-    TopicArn: 'arn:aws:sns:eu-central-1:980592027291:news',
-    EndPoint: 'abc.com'
-}, function(error, data) {
-    console.log(error || data);
-})
-
-function createHttpServer() {
-    var server = new http.Server();
-    server.on( 'request', function( request, response ){
-        request.setEncoding( 'utf8' );
-
-        //concatenate POST data
-        var msgBody = '';
-        request.on( 'data', function( data ){
-            msgBody += data;
-        });
-        request.on( 'end', function(){
-            var msgData = parseJSON( msgBody );
-            var msgType = request.headers[ 'x-amz-sns-message-type' ];
-            handleIncomingMessage( msgType, msgData );
-        });
-
-        // SNS doesn't care about our response as long as it comes
-        // with a HTTP statuscode of 200
-        response.end( 'OK' );
-    });
-
-    server.listen( 6001, subscribeToSnS );
-}
-
-function handleIncomingMessage( msgType, msgData ) {
-    if( msgType === 'SubscriptionConfirmation') {
-        //confirm the subscription.
-        sns.confirmSubscription({
-            Token: msgData.Token,
-            TopicArn: msgData.TopicArn
-        }, onAwsResponse );
-    } else if ( msgType === 'Notification' ) {
-        // That's where the actual messages will arrive
-    } else {
-        console.log( 'Unexpected message type ' + msgType );
-    }
-}
+server.listen(process.env.PORT || 8085);
