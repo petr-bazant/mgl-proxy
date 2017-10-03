17f854bef22b33fb9f61a971fe017137b9f82b89
petr-bazant <petr.bazant@outlook.com>
2017-10-03 12:49:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 var http = require('http');
 var server = http.createServer(function(req, res){
-	if(req.headers['host'].includes('.cz')) {
-		res.writeHead(301, {'Location' : 'http://www.100pge.com/cz'});
-		res.end(); 
-	} else {
-		res.writeHead(301, {'Location' : 'http://www.100pge.com/en'});
-		res.end(); 
-	}
+	res.writeHead(301, {'Location' : 'http://en.mygreenlife.info'});
 });
-server.listen(process.env.PORT || 8085);
+server.listen(process.env.PORT || 8086);
