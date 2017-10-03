df8ca6d78c477fc1d714e3ed0bcc348a89323e97
petr-bazant <petr.bazant@outlook.com>
2017-10-03 13:30:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var server = http.createServer(function(req, res){
 	res.writeHead(301, {'Location' : 'http://en.mygreenlife.info'});
+	res.end();
 });
 server.listen(process.env.PORT || 8086);
